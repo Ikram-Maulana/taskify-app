d5fe6b6bde284a3db7ec7717a00134bcbc7784fb
ikram-maulana <ikram075@ummi.ac.id>
2022-09-06 11:59:18 +0700

    Feat(Logic): Todo Complete Functionality
    - Membuat logic complete functionality
    - Menerapkan logic complete functionality

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -9,9 +9,23 @@ interface Props {
 }
 
 const TodoItem: FC<Props> = ({ todo, todos, setTodos }) => {
+  const onDoneHandler = (id: number) => {
+    setTodos(
+      todos.map((todo) => 
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
   return (
     <form className="flex w-full md:w-[46%] lg:w-[31%] rounded-md p-5 mb-2 md:mb-4 border border-gray-200 bg-white hover:shadow-md hover:transition-all">
-      <p className="flex-1 p-1 border-none">{todo.todo}</p>
+      <p
+        className={`flex-1 p-1 border-none ${
+          todo.isDone ? "line-through decoration-2" : ""
+        }`}
+      >
+        {todo.todo}
+      </p>
 
       <div className="flex gap-3 place-items-center text-lg">
         <span className="cursor-pointer">
@@ -20,7 +34,7 @@ const TodoItem: FC<Props> = ({ todo, todos, setTodos }) => {
         <span className="cursor-pointer">
           <FaTrash />
         </span>
-        <span className="cursor-pointer">
+        <span className="cursor-pointer" onClick={() => onDoneHandler(todo.id)}>
           <FaCheck />
         </span>
       </div>
