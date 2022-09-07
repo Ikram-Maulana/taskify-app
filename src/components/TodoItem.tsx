b6016eef061118893225f7dd2399c086ddafb53a
ikram-maulana <ikram075@ummi.ac.id>
2022-09-07 18:08:37 +0700

    Feat(Ctx): Draggable Tag
    - Delete `React.StrictMode` bug for `React Beautiful DnD`
    - Implement `Draggable` on `TodoItem.tsx`
    - Add props `draggableId="idName"` and `index={index}` to `Draggable` on `TodoItem.tsx`
    - Wrap the `div` of UI to `callback` => {(provided) => (`div`)}
    - On the `div` parent gave the `props` => `ref={provided.innerRef}`, `{...provided.draggableProps}` and `{...provided.dragHandleProps}`

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -6,6 +6,7 @@ import {
   useEffect,
   useRef
 } from "react";
+import { Draggable } from "react-beautiful-dnd";
 import { confirmAlert } from "react-confirm-alert";
 import { FaCheck, FaEdit, FaTrash, FaWindowClose } from "react-icons/fa";
 import charLimiter from "../utils/CharLimiter";
@@ -102,102 +103,113 @@ const TodoItem: FC<Props> = ({
   }, [edit]);
 
   return (
-    <form
-      className="flex w-full rounded-md p-5 mb-2 md:mb-4 border border-gray-200 bg-white hover:scale-105  lg:hover:scale-[1.02]  hover:drop-shadow-lg transition-all"
-      onSubmit={(e) =>
-        edit
-          ? notifyWarn("Can't edit other tasks, when you're in editing mode!")
-          : onTaskEditSubmitHandler(e, todo.id)
-      }
-    >
-      <div className="w-full">
-        {todo.isEdit ? (
-          <input
-            type="text"
-            id="visitors"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-11/12 p-2.5"
-            value={editTodo}
-            ref={inputRef}
-            onChange={(e) => onTaskEditChangeHandler(e.target.value)}
-          ></input>
-        ) : (
-          <p
-            className={`flex-1 p-1 border-none ${
-              todo.isDone ? "line-through decoration-2" : ""
-            }`}
-          >
-            {todo.todo}
-          </p>
-        )}
-      </div>
-
-      <div className="flex gap-3 place-items-center text-lg">
-        {!todo.isEdit ? (
-          <span
-            className={`${
-              edit ? "cursor-not-allowed" : "cursor-pointer"
-            } has-tooltip`}
-            onClick={(e) => {
-              e.preventDefault();
-              if (!todo.isDone && !edit) {
-                setEditTodo(todo.todo);
-                setEdit(true);
-                todo.isEdit = true;
-                setCharRemaining(charLimit - todo.todo.length);
-                setDisabled(true);
-                setTodo("");
-              } else {
-                notifyWarn(
-                  "Can't edit other tasks, when you're in editing mode!"
-                );
-              }
-            }}
-          >
-            <Tooltip>Edit</Tooltip>
-            <FaEdit />
-          </span>
-        ) : (
-          <span
-            className={`cursor-pointer has-tooltip ${edit && "text-red-700"}`}
-            onClick={(e) => {
-              e.preventDefault();
-              if (!todo.isDone) {
-                setEditTodo("");
-                setEdit(false);
-                todo.isEdit = false;
-                setCharRemaining(charLimit);
-                setDisabled(false);
-              }
-            }}
-          >
-            <Tooltip>Cancel</Tooltip>
-            <FaWindowClose />
-          </span>
-        )}
-        <span
-          className="cursor-pointer has-tooltip"
-          onClick={() => onDeleteHandler(todo.id)}
-        >
-          <Tooltip>Delete</Tooltip>
-          <FaTrash />
-        </span>
-        <span
-          className={`${
-            edit ? "cursor-not-allowed" : "cursor-pointer"
-          } has-tooltip`}
-          onClick={() =>
+    <Draggable draggableId={todo.id.toString()} index={index}>
+      {(provided) => (
+        <form
+          className="flex w-full rounded-md p-5 mb-2 md:mb-4 border border-gray-200 bg-white hover:scale-105  lg:hover:scale-[1.02]  hover:drop-shadow-lg transition-all"
+          onSubmit={(e) =>
             edit
               ? notifyWarn(
-                  "Can't mark as done tasks, when you're in editing mode!"
+                  "Can't edit other tasks, when you're in editing mode!"
                 )
-              : onDoneHandler(todo.id)
+              : onTaskEditSubmitHandler(e, todo.id)
           }
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
         >
-          <Tooltip>Done</Tooltip>
-          <FaCheck />
-        </span>
-      </div>
-    </form>
+          <div className="w-full">
+            {todo.isEdit ? (
+              <input
+                type="text"
+                id="visitors"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-11/12 p-2.5"
+                value={editTodo}
+                ref={inputRef}
+                onChange={(e) => onTaskEditChangeHandler(e.target.value)}
+              ></input>
+            ) : (
+              <p
+                className={`flex-1 p-1 border-none ${
+                  todo.isDone ? "line-through decoration-2" : ""
+                }`}
+              >
+                {todo.todo}
+              </p>
+            )}
+          </div>
+
+          <div className="flex gap-3 place-items-center text-lg">
+            {!todo.isEdit ? (
+              <span
+                className={`${
+                  edit ? "cursor-not-allowed" : "cursor-pointer"
+                } has-tooltip`}
+                onClick={(e) => {
+                  e.preventDefault();
+                  if (!todo.isDone && !edit) {
+                    setEditTodo(todo.todo);
+                    setEdit(true);
+                    todo.isEdit = true;
+                    setCharRemaining(charLimit - todo.todo.length);
+                    setDisabled(true);
+                    setTodo("");
+                  } else {
+                    notifyWarn(
+                      "Can't edit other tasks, when you're in editing mode!"
+                    );
+                  }
+                }}
+              >
+                <Tooltip>Edit</Tooltip>
+                <FaEdit />
+              </span>
+            ) : (
+              <span
+                className={`cursor-pointer has-tooltip ${
+                  edit && "text-red-700"
+                }`}
+                onClick={(e) => {
+                  e.preventDefault();
+                  if (!todo.isDone) {
+                    setEditTodo("");
+                    setEdit(false);
+                    todo.isEdit = false;
+                    setCharRemaining(charLimit);
+                    setDisabled(false);
+                  }
+                }}
+              >
+                <Tooltip>Cancel</Tooltip>
+                <FaWindowClose />
+              </span>
+            )}
+            <span
+              className="cursor-pointer has-tooltip"
+              onClick={() => onDeleteHandler(todo.id)}
+            >
+              <Tooltip>Delete</Tooltip>
+              <FaTrash />
+            </span>
+            <span
+              className={`${
+                edit ? "cursor-not-allowed" : "cursor-pointer"
+              } has-tooltip`}
+              onClick={() =>
+                edit
+                  ? notifyWarn(
+                      "Can't mark as done tasks, when you're in editing mode!"
+                    )
+                  : onDoneHandler(todo.id)
+              }
+            >
+              <Tooltip>Done</Tooltip>
+              <FaCheck />
+            </span>
+          </div>
+        </form>
+      )}
+    </Draggable>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -11,20 +11,18 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <DragDropContext onDragEnd={() => {}}>
-      <App />
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable={false}
-        pauseOnHover={false}
-      />
-    </DragDropContext>
-  </React.StrictMode>
+  <DragDropContext onDragEnd={() => {}}>
+    <App />
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable={false}
+      pauseOnHover={false}
+    />
+  </DragDropContext>
 );
